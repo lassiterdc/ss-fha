50167f0c77a1b072ebc0ed146199cdb501f6ea2f
Daniel Lassiter <daniel.lassiter@outlook.com>
2026-02-25 15:38:29 -0500

    squeeze dims of case study data so their coords are all identical

--- _work/exporting_case_study_data_files.py
+++ _work/exporting_case_study_data_files.py
@@ -11,6 +11,11 @@ reindexed_ensemble = Path(
     "/mnt/d/Dropbox/_GradSchool/_norfolk/_sharing/2025-06-30_research material for Aashutosh/simulation_results/reindexed_with_flood_probs/ensemble_results_reindexed.zarr"
 )
 
+reindexed_ensemble_obs = Path(
+    "/mnt/d/Dropbox/_GradSchool/_norfolk/_sharing/2025-06-30_research material for Aashutosh/simulation_results/reindexed_with_flood_probs/flood_probs_obs.zarr"
+)
+
+
 hydroshare_datadir = Path("/mnt/d/Dropbox/_GradSchool/repos/ss-fha/hydroshare_data")
 
 model_results = hydroshare_datadir / "model_results"
@@ -33,53 +38,65 @@ def return_dic_zarr_encodings_v2(ds: xr.Dataset, clevel: int = 5) -> dict:
     return encoding
 
 
-# %%
+# %% observed
+da = xr.open_dataset(
+    reindexed_ensemble_obs, engine="zarr", chunks="auto", consolidated=False
+)["max_wlevel_m"]
+
+# triton only
+ds = (da.rename(dict(event_number="event_iloc"))).to_dataset()
+fname_out = model_results / "obs_tritonswmm_combined.zarr"
+# tsut.write_zarr(ds, fname_out, compression_level=5)
+encoding = return_dic_zarr_encodings_v2(ds)
+ds.to_zarr(fname_out, mode="w", encoding=encoding, consolidated=False, zarr_format=2)
+
+
+# %% ss
 da = xr.open_dataset(
     reindexed_ensemble, engine="zarr", chunks="auto", consolidated=False
 )["max_wlevel_m"]
 
 # triton only
 ds = (
-    da.sel(dict(ensemble_type="2D_compound"))
+    da.sel(dict(ensemble_type=["2D_compound"]))
     .squeeze(drop=True)
     .rename(dict(event_number="event_iloc"))
 ).to_dataset()
-fname_out = model_results / "triton_only_ensemble.zarr"
+fname_out = model_results / "ss_triton_only_combined.zarr"
 # tsut.write_zarr(ds, fname_out, compression_level=5)
 
 encoding = return_dic_zarr_encodings_v2(ds)
 ds.to_zarr(fname_out, mode="w", encoding=encoding, consolidated=False, zarr_format=2)
 
-# %%
 # tritonswmm_rain_only
 ds = (
-    da.sel(dict(ensemble_type="rain_only"))
+    da.sel(dict(ensemble_type=["rain_only"]))
     .squeeze(drop=True)
     .rename(dict(event_number="event_iloc"))
 ).to_dataset()
-fname_out = model_results / "tritonswmm_ensemble_rain_only.zarr"
+fname_out = model_results / "ss_tritonswmm_rainonly.zarr"
 # tsut.write_zarr(ds, fname_out, compression_level=5)
 encoding = return_dic_zarr_encodings_v2(ds)
 ds.to_zarr(fname_out, mode="w", encoding=encoding, consolidated=False, zarr_format=2)
 
 # tritonswmm_surge_only
 ds = (
-    da.sel(dict(ensemble_type="surge_only"))
+    da.sel(dict(ensemble_type=["surge_only"]))
     .squeeze(drop=True)
     .rename(dict(event_number="event_iloc"))
 ).to_dataset()
-fname_out = model_results / "tritonswmm_ensemble_surge_only.zarr"
+fname_out = model_results / "ss_tritonswmm_surgeonly.zarr"
 # tsut.write_zarr(ds, fname_out, compression_level=5)
 encoding = return_dic_zarr_encodings_v2(ds)
 ds.to_zarr(fname_out, mode="w", encoding=encoding, consolidated=False, zarr_format=2)
 
 # tritonswmm_combined
 ds = (
-    da.sel(dict(ensemble_type="compound"))
+    da.sel(dict(ensemble_type=["compound"]))
     .squeeze(drop=True)
     .rename(dict(event_number="event_iloc"))
 ).to_dataset()
-fname_out = model_results / "tritonswmm_ensemble.zarr"
+fname_out = model_results / "ss_tritonswmm_combined.zarr"
 # tsut.write_zarr(ds, fname_out, compression_level=5)
 encoding = return_dic_zarr_encodings_v2(ds)
 ds.to_zarr(fname_out, mode="w", encoding=encoding, consolidated=False, zarr_format=2)
@@ -159,11 +176,11 @@ ds_dsgn_tseries.sel(event_type="surge")["waterlevel_m"].to_dataframe().dropna()[
 
 # %% subset model results and weather
 # combined
-rain_duration_h = 24
-event_type = "compound"
-simtype = "compound"
-model = "tritonswmm"
-scen_type = "combined"
+rain_duration_h = [24]
+event_type = ["compound"]
+simtype = ["compound"]
+model = ["tritonswmm"]
+scen_type = ["combined"]
 ds = (
     ds_triton_dsgn.sel(
         rain_duration_h=rain_duration_h,
@@ -174,20 +191,20 @@ ds = (
     .squeeze(drop=True)["max_wlevel_m"]
     .to_dataset()
 )
-f_out = model_results / f"design_storm_tritonswmm_{scen_type}.zarr"
+f_out = model_results / f"design_storm_tritonswmm_{scen_type[0]}.zarr"
 encoding = return_dic_zarr_encodings_v2(ds)
 ds.to_zarr(f_out, mode="w", encoding=encoding, consolidated=False, zarr_format=2)
 
 ds = ds_dsgn_tseries.sel(rain_duration_h=rain_duration_h, event_type=event_type)
-f_out = weather / f"design_storm_{scen_type}.nc"
+f_out = weather / f"design_storm_{scen_type[0]}.nc"
 ds.to_netcdf(f_out)
 
 #  rain only
-rain_duration_h = 24
-event_type = "rain"
-simtype = "compound"
-model = "tritonswmm"
-scen_type = "rainonly"
+rain_duration_h = [24]
+event_type = ["rain"]
+simtype = ["compound"]
+model = ["tritonswmm"]
+scen_type = ["rainonly"]
 ds = (
     ds_triton_dsgn.sel(
         rain_duration_h=rain_duration_h,
@@ -198,12 +215,12 @@ ds = (
     .squeeze(drop=True)["max_wlevel_m"]
     .to_dataset()
 )
-f_out = model_results / f"design_storm_tritonswmm_{scen_type}.zarr"
+f_out = model_results / f"design_storm_tritonswmm_{scen_type[0]}.zarr"
 encoding = return_dic_zarr_encodings_v2(ds)
 ds.to_zarr(f_out, mode="w", encoding=encoding, consolidated=False, zarr_format=2)
 
 ds = ds_dsgn_tseries.sel(rain_duration_h=rain_duration_h, event_type=event_type)
-f_out = weather / f"design_storm_{scen_type}.nc"
+f_out = weather / f"design_storm_{scen_type[0]}.nc"
 ds.to_netcdf(f_out)
 
 
@@ -212,11 +229,11 @@ ds.to_netcdf(f_out)
 # ).to_dataframe()[["mm_per_hr", "waterlevel_m"]].dropna()
 
 # surge only
-rain_duration_h = 6
-event_type = "surge"
-simtype = "compound"
-model = "tritonswmm"
-scen_type = "surgeonly"
+rain_duration_h = [6]
+event_type = ["surge"]
+simtype = ["compound"]
+model = ["tritonswmm"]
+scen_type = ["surgeonly"]
 ds = (
     ds_triton_dsgn.sel(
         rain_duration_h=rain_duration_h,
@@ -227,12 +244,12 @@ ds = (
     .squeeze(drop=True)["max_wlevel_m"]
     .to_dataset()
 )
-f_out = model_results / f"design_storm_tritonswmm_{scen_type}.zarr"
+f_out = model_results / f"design_storm_tritonswmm_{scen_type[0]}.zarr"
 encoding = return_dic_zarr_encodings_v2(ds)
 ds.to_zarr(f_out, mode="w", encoding=encoding, consolidated=False, zarr_format=2)
 
 ds = ds_dsgn_tseries.sel(rain_duration_h=rain_duration_h, event_type=event_type)
-f_out = weather / f"design_storm_{scen_type}.nc"
+f_out = weather / f"design_storm_{scen_type[0]}.nc"
 ds.to_netcdf(f_out)
 
 
